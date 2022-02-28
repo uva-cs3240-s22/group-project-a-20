d92d8b307019699319d5aa216edd7d68eec0e5e4
Spencer <78666180+SpencerP23@users.noreply.github.com>
2022-02-28 13:40:23 -0500

    including templates

--- WoM/settings.py
+++ WoM/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'recipes.apps.RecipesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def home(request):
-    return HttpResponse("Hello, home page! Go to /login to log in")
+    name = "you are not logged in"
+    context = {
+        "name" : name,
+        }
+    template = loader.get_template('recipes/home.html')
+    return HttpResponse(template.render(context, request))
 
 #temp pre-login
 def pre(request):
-    return HttpResponse("Hello, you are not logged in yet")
+    name = "you are not logged in"
+    context = {
+        "name" : name,
+        }
+    template = loader.get_template('recipes/login.html')
+    return HttpResponse(template.render(context, request))
 
 #temp post-login
 def post(request):
-    return HttpResponse("Hello, you are logged in!")
+    name = "you are not logged in"
+    context = {
+        "name" : name,
+        }
+    template = loader.get_template('recipes/loggedin.html')
+    return HttpResponse(template.render(context, request))
