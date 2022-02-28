560ab69bbce59e35b1552a286de0b141f855f895
HelsEch <jkn3pk@virginia.edu>
2022-02-28 16:37:50 -0500

    a little documentation added

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login', views.login, name = 'login'),
     path('loggedIn', views.loggedIn, name = 'loggedIn'),
+    # for account authentication
     path('accounts/', include('allauths.urls')),
 ]
