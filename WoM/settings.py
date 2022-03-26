dceaefb7602299c1091162b677987695378bb0b8
HelsEch <jkn3pk@virginia.edu>
2022-03-26 15:02:37 -0400

    Update settings.py

--- WoM/settings.py
+++ WoM/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-#import django_heroku
+import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
