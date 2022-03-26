8ec87554fb0cdc070fffe78c804cab6bc794dfe1
HelsEch <jkn3pk@virginia.edu>
2022-03-26 14:07:09 -0400

    fixed issue with home links

--- WoM/urls.py
+++ WoM/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('recipes.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('recipes.urls')),
 ]
