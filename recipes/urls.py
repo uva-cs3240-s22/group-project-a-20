5722ace2defa1b49ec69c8e7edbcf03ce18e26bc
HelsEch <jkn3pk@virginia.edu>
2022-03-26 14:39:21 -0400

    url update

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login', views.login, name = 'login'),
     path('loggedIn', views.loggedIn, name = 'loggedIn'),
-    path('recipe/form/', views.get_recipe, name = 'recipeform'),
-    path('recipe/list/', views.RecipeListView.as_view(), name = 'recipelist'),
-    path('recipe/<int:pk>/', views.RecipeView.as_view(), name = 'recipe'),
+    path('recipes/form/', views.get_recipe, name = 'recipeform'),
+    path('recipes/list/', views.RecipeListView.as_view(), name = 'recipelist'),
+    path('recipes/<int:pk>/', views.RecipeView.as_view(), name = 'recipe'),
     # for account authentication
     path('accounts/', include('allauth.urls')),
 ]
