692208d0191108946a51b523fd228c7d4e791efb
Spencer <78666180+SpencerP23@users.noreply.github.com>
2022-03-27 15:25:43 -0400

    idk probably ignore this branch I just started to do what it said

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# from https://levelup.gitconnected.com/creating-a-recipe-api-with-django-rest-framework-6c2512d71d5b
+class recipe(models.Model): #declaration of class
+ #diffeerent fields
+    name = models.CharField(max_length=400)
+    ingriedient = models.CharField(max_length=1000)
+    time=models.IntegerField()
+    process= models.CharField(max_length=2000)
+#function to return the name of record as name
+    def __str__(self):
+        return self.name
