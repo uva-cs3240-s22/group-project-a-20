655f7414c113e1633caa2343400ab6207f33d13b
HelsEch <jkn3pk@virginia.edu>
2022-04-02 15:32:14 -0400

    Ingredients submittable

--- recipes/models.py
+++ recipes/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 
 class Recipe(models.Model):
     recipe_title = models.CharField(max_length=100)
-    recipe_ingredients = models.ArrayField(base_field=models.CharField(max_length=100))
+    recipe_ingredients = ArrayField(base_field=models.CharField(max_length=100), default=list)
     recipe_instructions = models.CharField(max_length=5000)
     pub_date = models.DateTimeField(auto_now_add=True)
