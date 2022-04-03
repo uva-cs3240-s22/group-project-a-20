73203d4a0701eaaf2217fd6318e9bc7ed3b2e225
Teddy Walsh <98552735+teddy-walsh@users.noreply.github.com>
2022-04-02 21:12:12 -0400

    Model accepts img
    Model adds field to accept image. Also sets up static files to pull from S3

--- WoM/settings.py
+++ WoM/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+    #AWS
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -139,12 +141,41 @@ USE_I18N = True
 
 USE_TZ = True
 
+# AWS VILLAGE
+AWS_ACCESS_KEY_ID = 'AKIAXQ5MPBEQSPMNK5XR'
+
+AWS_SECRET_ACCESS_KEY = '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'
+
+AWS_STORAGE_BUCKET_NAME = 'word-of-mouth-a20'
+
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+AWS_DEFAULT_ACL = 'public-read'
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400'
+}
+
+AWS_LOCATION = 'static'
+
+AWS_QUERYSTRING_AUTH = False;
+
+AWS_HEADERS = {
+    'Access-Control-Allow-Origin': '*',
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
