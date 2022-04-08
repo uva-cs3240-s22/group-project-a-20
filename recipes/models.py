a53cd21a0caf490248607113691126396b1669c6
HelsEch <jkn3pk@virginia.edu>
2022-04-07 20:03:29 -0400

    Updated database and Recipe Model

--- recipes/models.py
+++ recipes/models.py
@@ -5,6 +5,6 @@ from django.contrib.postgres.fields import ArrayField
 
 class Recipe(models.Model):
     recipe_title = models.CharField(max_length=100)
-    recipe_ingredients = ArrayField(base_field=models.CharField(max_length=100), default=list)
+    recipe_ingredients = ArrayField(base_field=models.CharField(max_length=100), null=True)
     recipe_instructions = models.CharField(max_length=5000)
     pub_date = models.DateTimeField(auto_now_add=True)
