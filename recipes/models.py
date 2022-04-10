6a7f0c28eb3546334b982474659d5ed58605742d
HelsEch <jkn3pk@virginia.edu>
2022-04-10 13:20:26 -0400

    socialaccount in recipe

--- recipes/models.py
+++ recipes/models.py
@@ -14,6 +14,7 @@ class Recipe(models.Model):
         blank=True,
         null=True,
     )
+    user = models.ForeignKey('socialaccount.SocialAccount', on_delete=models.CASCADE, )
     img = models.ImageField(upload_to='recipe_images/', blank=True, null=True)
 
     def __str__(self):
