d6bccc1db44797eacb8d1ef68d84f7322ad5c480
HelsEch <jkn3pk@virginia.edu>
2022-04-16 17:06:29 -0400

    Update 0003_profile.py

--- recipes/migrations/0003_profile.py
+++ recipes/migrations/0003_profile.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('recipes', '0002_alter_recipe_pub_date'),
+        ('recipes', '0002_alter_recipe_author'),
     ]
 
     operations = [
