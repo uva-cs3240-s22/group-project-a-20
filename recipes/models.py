4d874f7d70eb87d840554095811305b61d679392
HelsEch <jkn3pk@virginia.edu>
2022-04-21 22:59:39 -0400

    Update models.py

--- recipes/models.py
+++ recipes/models.py
@@ -16,11 +16,11 @@ class Recipe(models.Model):
     )
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     img = models.ImageField(upload_to='recipe_images/', blank=True, null=True)
-    '''favorite = models.ManyToManyField(
+    favorite = models.ManyToManyField(
         User, 
         blank=True,
         null=True,
-    )'''
+    )
 
     def __str__(self):
         return self.recipe_title
